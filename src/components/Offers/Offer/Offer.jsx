85909081ade530f1bfd3fb28212932ba25a5028a
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-10-29 20:01:09 +0700

    add other offer in offerdetail page

--- src/components/Offers/Offer/Offer.jsx
+++ src/components/Offers/Offer/Offer.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
 
-import { ButtonBase } from '@mui/material'
 import { offerUrl } from '../../../constants/pathUrl';
 import Thumbnail from '../../Thumbnail/Thumbnail.jsx';
-import muiStyles from './muiStyles.js';
 
 const Offer = ({ offer }) => {
 
-    const navigate = useNavigate();
-    
-    const openOffer = () => navigate(`${offerUrl}/details/${offer._id}`);
-
     return (
-        <ButtonBase style={muiStyles.cardAction} className="hover:bg-gray-300/60" onClick={openOffer}>
-            <Thumbnail imgW="270px" imgH="340px" imgSrc={offer.mainImg} imgAlt="chandra service" head={offer.title} detail={offer.detail}/>
-        </ButtonBase>
+        <div key={offer._id}>
+            <Thumbnail imgW="270px" imgH="340px" imgSrc={offer.mainImg} imgAlt="chandra service" head={offer.title} detail={offer.detail} linkTo={`${offerUrl}/details/${offer._id}`} />
+        </div>
     )
 }
 
